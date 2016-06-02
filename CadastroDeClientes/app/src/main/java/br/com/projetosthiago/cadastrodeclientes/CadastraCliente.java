1f31914433617b706ab6eb285fc2e4a7b15cc6b3
Thiago Morais <thiagomorais@projetosthiago.com.br>
2016-06-02 09:24:00 -0300

    Commitando o projeto incial

---
+++ CadastroDeClientes/app/src/main/java/br/com/projetosthiago/cadastrodeclientes/CadastraCliente.java
@@ -0,0 +1,13 @@
+package br.com.projetosthiago.cadastrodeclientes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CadastraCliente extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastra_cliente);
+    }
+}
